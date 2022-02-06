fe054a6fe0f49f5aca9727b772b6346467615253
Huggyturd <cleitonmloura@gmail.com>
2022-02-05 23:07:21 -0300

    Modificações: - Adicionado mkdir para pastas

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 CURRENT_DIRECTORY=`pwd`
 echo "DATALAKE_PATH="$CURRENT_DIRECTORY > .env
+mkdir raw_zone stage_zone refined_zone dags logs plugins
 chmod -R 777 raw_zone stage_zone refined_zone dags logs plugins
 docker build --no-cache . -f Dockerfile --tag airflow-raizen-challenge:0.0.1
 docker-compose up -d
